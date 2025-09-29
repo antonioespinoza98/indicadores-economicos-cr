13bee92e27c2c1a900ab6ee08304848916662b1c
Marco Espinoza <espinozamarco70@gmail.com>
2025-09-29 11:36:27 +0200

    Estructuración del proyecto. Se agregaron tres carpetas: sample para los archivos principales, tests para los archivos de testing del código, y docs para la documentación del proyecto.

---
+++ sample/core.py
@@ -0,0 +1,6 @@
+# -------------------------------------------------------------------------------------
+#  Autor: Marco Espinoza — Consultor 
+# Laboratorio de Prospectiva, Innovación e Inteligencia Artificial
+# Fecha: 29-09-2025
+# Descripción del archivo: funciones importantes que no tienen depedencias de proyecto
+# -------------------------------------------------------------------------------------
---
+++ sample/helpers.py
@@ -0,0 +1,6 @@
+# ----------------------------------------------------------------
+#  Autor: Marco Espinoza — Consultor 
+# Laboratorio de Prospectiva, Innovación e Inteligencia Artificial
+# Fecha: 29-09-2025
+# Descripción del archivo: funciones complejas y/o específicas
+# ----------------------------------------------------------------
