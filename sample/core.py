d5f6a2e485db35aa7a2c069eaba4efa3484f6598
Marco Espinoza <espinozamarco70@gmail.com>
2025-09-29 17:31:14 +0200

    Se comenzó a automatizar la primera captura de datos del BCCR aprovechando el API. También se agregaron unas funciones en utils.py para leer la variable de entorno de Token, que sirve para ingresar a los indicadores.

--- sample/core.py
+++ sample/core.py
@@ -4,3 +4,56 @@
 # Fecha: 29-09-2025
 # Descripción del archivo: funciones importantes que no tienen depedencias de proyecto
 # -------------------------------------------------------------------------------------
+
+import configparser
+import requests
+import os
+from dotenv import load_dotenv
+import polars
+from sample.utils import TOKEN
+class BccrAPI:
+    """
+    Clase que se encarga de la captura de datos económicos del Banco Central de Costa Rica (BCCR).
+
+    ...
+
+    Atributos
+    ----------
+    API : str
+        Nombre de la API a utilizar
+
+    Métodos
+    ----------
+    BccrAPI("BCCR").get()
+        Extrae la tabla de indicadores económicos 
+    """
+    def __init__(self, API):
+        """
+        Parámetros
+        ----------
+        API : str
+            Nombre de la API a utilizar
+        """
+        config = configparser.ConfigParser()
+        config.read(os.path.dirname(__file__) + '/conf.ini')
+        self.url = config.get(API, 'url')
+        self.endpoint = config.get(API, 'endpoint')
+        self.header = {"authorization": f"Bearer {TOKEN}",
+                        "Content-Type": "application/json",
+                          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+                          }
+
+    def get(self):
+        """
+        Envía una solicitud GET
+
+        Raises
+        ----------
+        Error: 401
+            Si la conexión con el API no pudo ser establecida. 
+        """
+        response = requests.get(self.url + self.endpoint, headers= self.header)
+        if response.status_code == 200:
+            print("Funcioné!")
+        else: 
+            print(f"Error: {response.status_code}")
--- sample/utils.py
+++ sample/utils.py
@@ -4,3 +4,11 @@
 # Fecha: 29-09-2025
 # Descripción del archivo: funciones simples o/y genéricas
 # ----------------------------------------------------------------
+
+# --- Exportar la variable de entorno para el API
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
